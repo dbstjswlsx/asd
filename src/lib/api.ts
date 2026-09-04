1063564536fa8c1ca9ce5773ba2e939b68a2a77a
yoonsunjin <sjyundosan0223@jnu.ac.kr>
2026-09-04 21:21:00 +0900

    test

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://vibe.aitestbed.kr/preview/289ffc01-898a-4ab0-a019-2b9e8ae08983/api'
+const API_BASE_URL = 'https://vibe.aitestbed.kr/preview/1a450a3d-c9c5-4f6c-abed-da6ebdba5c27/api'
 
 export const api = (path: string, init?: RequestInit) =>
   fetch(`${API_BASE_URL}/${path.replace(/^\/+/, '')}`, init)
