2f4a01b7c336b62e9bec516da20919eee479e110
yoonsunjin <sjyundosan0223@jnu.ac.kr>
2026-09-04 21:16:51 +0900

    feat: api url 변경

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,2 +1,4 @@
+const API_BASE_URL = 'https://vibe.aitestbed.kr/preview/289ffc01-898a-4ab0-a019-2b9e8ae08983/api'
+
 export const api = (path: string, init?: RequestInit) =>
-  fetch(import.meta.env.BASE_URL + 'api/' + path.replace(/^\/+/, ''), init)
+  fetch(`${API_BASE_URL}/${path.replace(/^\/+/, '')}`, init)
